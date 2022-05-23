fdcffd86973776bd9491e874dd81ed9a704cadfe
Francisco Ramos Ruiz <franciscoramosruiz@MacBook-Pro-de-Francisco.local>
2022-05-24 04:15:06 +0900

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,9 +4,9 @@ module SessionsHelper
   end
 
   def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
-    end
+      if session[:user_id]
+        @current_user ||= User.find_by(id: session[:user_id])
+      end
   end
 
     def logged_in?
@@ -16,5 +16,9 @@ module SessionsHelper
     def log_out
       reset_session
       @current_user = nil
+    end
+
+    def store_location
+      session[:forwarding_url] = request.original_url if request.get?
     end
 end
