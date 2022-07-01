b947c9aa60389cb1f71481922cb104d0580c9d69
Francisco Ramos Ruiz <franciscoramosruiz@MacBook-Pro-de-Francisco.local>
2022-07-02 05:16:39 +0900

    Use SSL and the Puma webserver in production

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_01_190621) do
+ActiveRecord::Schema.define(version: 2022_07_01_201602) do
 
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
@@ -112,7 +112,6 @@ ActiveRecord::Schema.define(version: 2022_07_01_190621) do
     t.text "explanation_en", default: "uncheck"
     t.text "pairing_en", default: "uncheck"
     t.string "wine_type_en", default: "uncheck"
-    t.text "type"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
