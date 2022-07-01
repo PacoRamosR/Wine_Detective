b1ffe9bc4874e1a45128c49f2ffd9565078f88cb
Francisco Ramos Ruiz <franciscoramosruiz@MacBook-Pro-de-Francisco.local>
2022-07-01 21:17:27 +0900

    footer int changed

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -7,27 +7,10 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should get home" do
-    get root_path
-    assert_response :success
-    assert_select "title", "Home | Wine Detective"
-  end
-
-  test "should get help" do
-    get help_path
-    assert_response :success
-    assert_select "title", "Help | Wine Detective"
-  end
-
+  
   test "should get about" do
     get about_path
     assert_response :success
     assert_select "title", "About | Wine Detective"
   end
-
-  test "should get contact" do
-    get contact_path
-    assert_response :success
-    assert_select "title", "Contact | Wine Detective"
-  end
-end
+end  
