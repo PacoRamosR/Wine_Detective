07bffaff36c68be94630fc3bc52aa02a2d46c46f
Francisco Ramos Ruiz <franciscoramosruiz@MacBook-Pro-de-Francisco.local>
2022-07-04 14:01:19 +0900

    fix error with dropdown

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,9 +3,10 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+// require bootstrap
+//= require bootstrap-sprockets 
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap-sprockets 
 //= require turbolinks
 //= require_tree .
 
@@ -13,8 +14,9 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "bootstrap"
 import "jquery"
-import "bootstrap";
+
 
 
 Rails.start()
