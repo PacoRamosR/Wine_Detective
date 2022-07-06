d2e8c1fd9285b6f13ff09e670f92dac6f8493e6f
Francisco Ramos Ruiz <franciscoramosruiz@MacBook-Pro-de-Francisco.local>
2022-07-06 19:09:35 +0900

    fixed contact form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,11 +7,5 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.new(params[:contact])
     @contact.request = request
-    if @contact.deliver
-      flash.now[:success] = 'Message sent!'
-    else
-      flash.now[:error] = 'Could not send message'
-      render :new
-    end
   end
 end
